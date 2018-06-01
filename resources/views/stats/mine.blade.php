c3123ab4d190803caf825300f47d2e43fe797989
Maximiliano Chiavassa <maximiliano.chia@gmail.com>
2018-05-31 23:48:39 -0300

    Change stats format

--- resources/views/stats/mine.blade.php
+++ resources/views/stats/mine.blade.php
@@ -8,33 +8,33 @@
 </div>
 
 <div class="card p-3 mb-3">
-    <h4 class="mb-4">Pronósticos enviados</h4>
-    <h1>
+    <h6 class="mb-4">Pronósticos enviados</h6>
+    <h3>
         <strong>{{$forecastsCount}}</strong>
         <small class="text-muted">({{$forecastComputedCount}} computados)</small>
-    </h1>
+    </h3>
 </div>
 
 <div class="card p-3 mb-3">
-    <h4 class="mb-4">Resultados acertados</h4>
-    <h1>
+    <h6 class="mb-4">Resultados acertados</h6>
+    <h3>
         <strong>{{$matchResultForecastsCount}}</strong>
         <small class="text-muted">({{number_format($matchResultForecastsPercentage, 0)}}%)</small>
-    </h1>
+    </h3>
 </div>
 
 <div class="card p-3 mb-3">
-    <h4 class="mb-4">Marcadores acertados</h4>
-    <h1>
+    <h6 class="mb-4">Marcadores acertados</h6>
+    <h3>
         <strong>{{$matchScoreForecastsCount}}</strong>
         <small class="text-muted">({{number_format($matchScoreForecastsPercentage, 0)}}%)</small>
-    </h1>
+    </h3>
 </div>
 
 <div class="card p-3 mb-3">
-    <h4 class="mb-4">No acertados</h4>
-    <h1>
+    <h6 class="mb-4">No acertados</h6>
+    <h3>
         <strong>{{$noMatchForecastsCount}}</strong>
         <small class="text-muted">({{number_format($noMatchForecastsPercentage, 0)}}%)</small>
-    </h1>
+    </h3>
 </div>
