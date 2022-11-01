b53a5d2ead384ad05b7efdab49c4aae7930aed55
Maximiliano Chiavassa <maximiliano.chia@gmail.com>
2022-11-01 14:46:07 -0300

    Typo

--- lang/es/stats.php
+++ lang/es/stats.php
@@ -4,7 +4,7 @@ return [
     'rankings_title' => 'Rankings',
     'top_users' => 'Top jugadores',
     'total_users' => ':total jugadores total',
-    'best_assertions' => 'Mejores asciertos',
+    'best_assertions' => 'Mejores aciertos',
     'top_parties' => 'Top grupos',
     'top_parties_detail' => 'con 2 o más jugadores',
     'points' => 'Puntos',
